e446e65660354a063e3cc18c7be086e0548dd3bc
Saba-Var <vartasashvili94@gmail.com>
2022-04-26 14:55:01 +0400

    chore(svg): add star svg to vaccinated page

--- src/Components/Image03.js
+++ src/Components/Image03.js
@@ -1,9 +1,18 @@
 import DoctorImg from '../Assets/images/doctor.png';
+import star from '../Assets/svg/star.svg';
+
 function Image03() {
   return (
-    <div className='absolute right-40 top-28 '>
-      <img src={DoctorImg} alt='doctor' className='pt-8' />
-    </div>
+    <>
+      <div className='absolute right-40 top-36 '>
+        <img src={DoctorImg} alt='doctor' className='pt-8' />
+      </div>
+      <img
+        src={star}
+        alt='star'
+        className='absolute right-[622px] top-40 mix-blend-darken'
+      />
+    </>
   );
 }
 
