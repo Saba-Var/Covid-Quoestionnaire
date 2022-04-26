40065abc1af91b1bad84fde51b7d73a935967ec8
Saba-Var <vartasashvili94@gmail.com>
2022-04-26 14:40:30 +0400

    fix(button): prevent button change location
    prevent submit button on the covid policy page to change its location when the user clicks

--- src/Pages/Covid-Policy/CovidPolicy.js
+++ src/Pages/Covid-Policy/CovidPolicy.js
@@ -49,32 +49,37 @@ function CovidPolicy() {
     e.preventDefault();
   };
   return (
-    <div className='bg-gray h-492'>
-      <Card>
-        <Header page='4' />
-        <p className='text-standard leading-7 w-176 mb-11'>
-          რედბერის მთავარი ღირებულება ჩვენი გუნდის თითოეული წევრია. გარემო,
-          რომელსაც ჩვენი თანამშრომლები ქმნით, ბევრისთვის არის და ყოფილა წლების
-          განმავლობაში მიზნებისთვის ერთად ბრძოლის მიზეზი, ბევრისთვის კი —
-          ჩვენთან გადმოსვლის.
-          <br /> <br /> პანდემიის პერიოდში ერთმანეთსაც იშვიათად ვნახულობთ
-          პირისპირ და ყოველდღიური კომუნიკაციაც გაიშვიათდა.
-        </p>
-        <form
-          className='flex flex-col gap-14 w-162'
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <Frequency register={register} errors={errors} />
-          <WorkDays register={register} errors={errors} />
-          <PhysicalGathering register={register} />
-          <Opinions register={register} />
-          <button className='ml-auto rounded-[42px] text-white px-7 py-4 w-44 bg-cyan-600 font-bold text-lg z-20 hover:bg-cyan-800'>
-            დასრულება
-          </button>
-        </form>
+    <div className='bg-gray'>
+      <div className='bg-gray h-492'>
+        <Card>
+          <Header page='4' />
+          <p className='text-standard leading-7 w-176 mb-11'>
+            რედბერის მთავარი ღირებულება ჩვენი გუნდის თითოეული წევრია. გარემო,
+            რომელსაც ჩვენი თანამშრომლები ქმნით, ბევრისთვის არის და ყოფილა წლების
+            განმავლობაში მიზნებისთვის ერთად ბრძოლის მიზეზი, ბევრისთვის კი —
+            ჩვენთან გადმოსვლის.
+            <br /> <br /> პანდემიის პერიოდში ერთმანეთსაც იშვიათად ვნახულობთ
+            პირისპირ და ყოველდღიური კომუნიკაციაც გაიშვიათდა.
+          </p>
+          <form
+            className='flex flex-col gap-14 w-162'
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <Frequency register={register} errors={errors} />
+            <WorkDays register={register} errors={errors} />
+            <PhysicalGathering register={register} />
+            <Opinions register={register} />
+            <button className='ml-auto rounded-[42px] text-white px-7 py-4 w-44 bg-cyan-600 font-bold text-lg z-20 hover:bg-cyan-800'>
+              დასრულება
+            </button>
+          </form>
+        </Card>
+        <Image04 />
+      </div>
+      <div className='h-8 mt-8 pt-8 pb-6'>
         <NavigationArrors back='/Vaccinated' top={'pt-248'} />
-      </Card>
-      <Image04 />
+      </div>
+      <div className='h-2'></div>
     </div>
   );
 }
