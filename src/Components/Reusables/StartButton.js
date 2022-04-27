0911170b8be675d365dc588392b928a277fa871f
Saba-Var <vartasashvili94@gmail.com>
2022-04-27 22:20:03 +0400

    chore: create start questionaire svg component

--- src/Components/Reusables/StartButton.js
+++ src/Components/Reusables/StartButton.js
@@ -1,16 +1,12 @@
 import StartingQuestionaireHover from 'Components/svgs/StartingQuestionaireHover.svg';
-import StartingQuestionnaire from 'Components/svgs/StartingQuestionnaire.svg';
+import StartingQuestionnaire from 'Components/svgs/StartingQuestionnaire';
 import { Link } from 'react-router-dom';
 
 function StartButton(props) {
   return (
     <>
       {!props.hover ? (
-        <img
-          src={StartingQuestionnaire}
-          alt='start questionnaire button'
-          className='p-4 pt-1 hover:cursor-pointer w-50'
-        />
+        <StartingQuestionnaire />
       ) : (
         <Link to='/identification'>
           <img
