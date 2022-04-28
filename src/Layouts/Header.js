0b923a5533be9b953b5b05de09c55e3b022470b5
Saba-Var <vartasashvili94@gmail.com>
2022-04-28 09:15:39 +0400

    chore: build Redberry text logo component

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,9 +1,9 @@
-import RedberryLogo from 'Components/svgs/RedberryLogo.svg';
+import RedberryLogoText from 'Components/svgs/RedberryLogoText';
 
 function Header(props) {
   return (
     <div className='bg-gray flex justify-between font-header font-bold items-center h-24 border-b-2 mb-10'>
-      <img src={RedberryLogo} alt='Redberry logo' className='w-36' />
+      <RedberryLogoText />
       <p className='text-normal font-anonymous'>{props.page}/4</p>
     </div>
   );
