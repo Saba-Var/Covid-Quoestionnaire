b8d6c21ee5d1803f3d73c8357d79cc8834b2020a
Saba-Var <vartasashvili94@gmail.com>
2022-05-02 16:01:41 +0400

    chore: add star icons to thanks page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,3 +4,4 @@ export { default as boyAndGirl } from './boy&girl.png';
 export { default as doctor } from './doctor.png';
 export { default as start } from './start.png';
 export { default as bike } from './bike.png';
+export { default as star } from './star.png';
--- src/pages/Thanks/Thanks.jsx
+++ src/pages/Thanks/Thanks.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-
+import { star } from 'assets/images';
 const Thanks = () => {
   const [redirectNow, setRedirectNow] = useState(false);
   useEffect(() => {
@@ -17,9 +17,17 @@ const Thanks = () => {
   }
   return (
     <div className='bg-charcoal h-screen w-full flex justify-center items-center'>
-      <h2 className='text-6xl font-bold text-white tracking-[0.24rem]'>
-        მადლობა
-      </h2>
+      <div className='relative'>
+        <img src={star} alt='star' className='absolute top-[-110%] left-[4%]' />
+        <h2 className='text-6xl font-bold text-white tracking-[0.4rem]'>
+          მადლობა
+        </h2>
+        <img
+          src={star}
+          alt='star'
+          className='w-8 h-8 absolute top-[120%] right-[8%]'
+        />
+      </div>
     </div>
   );
 };
